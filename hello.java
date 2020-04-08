3e69c46c1eef364edac6810879bb88cfa0505ed8
sriram <sriram@Srirams-MacBook-Pro.local>
2020-04-08 10:28:42 -0400

    test

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+        System.out.println("Java Project");
+        System.out.println("Under Jenkins");
+        System.out.println("DevOps testing");
+        System.out.println("Devops Tasks");
+        System.out.println("Devops Jobs");
+        System.out.println("This will be printed");
+    }
+}
